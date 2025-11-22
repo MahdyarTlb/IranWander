26c54e5016d917b9a45956e671eefa6c3cec985d
Ali <ali.arezoomandi1723@gmail.com>
2025-11-22 10:09:56 +0330

    Aziziii

--- iranwander/routes/auth.py
+++ iranwander/routes/auth.py
@@ -1,19 +1,19 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 
-auth = Blueprint('auth', __name__, template_folder='templates') 
+auth = Blueprint('auth', __name__, template_folder='templates/auth') 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    return render_template('auth/login.html')
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    return render_template('auth/signup.html')
 
 @auth.route('/forget', methods=['GET', 'POST'])
 def forget_password():
-    return render_template('forget.html')
+    return render_template('auth/forget.html')
 
 @auth.route('/set-password/<token>', methods=['GET', 'POST'])
 def set_password(token):
-    return render_template('setpassword.html', token=token)
+    return render_template('auth/setpassword.html', token=token)
